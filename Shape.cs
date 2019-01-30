2ddb897b427d2a64408e093df4ae2d198cb57030
Nazar Myrnyi <PiecesKNG@gmail.com>
2019-01-30 16:46:21 +0200

    interface that defines common methods for shapes

---
+++ Shape.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape
+{
+    interface IShape
+    {
+        void GetCoordinates();
+        void GetArea();
+        void GetPerimetr();
+        void Display();
+    }
+}
