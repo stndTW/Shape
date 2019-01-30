07a81739501f9cbb4f77de2cddc99a276d5cedc6
Nazar Myrnyi <PiecesKNG@gmail.com>
2019-01-30 16:43:10 +0200

    a class which, through reflection, creates the desired figure depending on the parameter (name) received

---
+++ ShapeGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape
+{
+    class ShapeGenerator
+    {
+        dynamic shape;
+        public ShapeGenerator(Shapes sh)
+        {
+            string name = GetType().Namespace + "." + sh;
+            shape = Activator.CreateInstance(Type.GetType(name));
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(new string('-', 50));
+            shape.Display();
+            Console.WriteLine(new string('-', 50));
+        }
+    }
+}
